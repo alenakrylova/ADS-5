5c0cac5441e29495e5fb442f03a846b850150a10
alena krylova <150517805+alenakrylova@users.noreply.github.com>
2024-04-13 16:44:39 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,44 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include "string"
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[300];
+  int top;
+
+ public:
+  TStack() : top(0) {}
+
+  void push(const T &item) {
+    if (top <= size - 1) {
+      data[top++] = item;
+    } else {
+      throw std::string("Stack overflow");
+    }
+  }
+
+  T pop() {
+    if (top > 0) {
+      return data[--top];
+    } else {
+      throw std::string("Stack is empty");
+    }
+  }
+
+  T get() const {
+    if (top > 0) {
+      return data[top - 1];
+    } else {
+      throw std::string("Empty");
+    }
+  }
+
+  bool isEmpty() const {
+    return top == 0;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
